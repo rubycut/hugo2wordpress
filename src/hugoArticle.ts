a5ddf52511a549a2b53f6052baa8b145702fd2c2
Hrvoje Marjanovic <hrvoje.marjanovic@gmail.com>
2018-04-15 14:59:09 +0200

    prettier done

--- src/hugoArticle.ts
+++ src/hugoArticle.ts
@@ -1,39 +1,39 @@
 class HugoArticle {
-  private slug: string;
-  private content: string;
-  private mdFilename: string;
-  private categories: string[];
-  private tags: string[];
+  private slug: string
+  private content: string
+  private mdFilename: string
+  private categories: string[]
+  private tags: string[]
   constructor(options) {
-    this.mdFilename = options.filename;
-    this.categories = [];
-    this.tags = [];
-    this.content = "";
-    this.slug = "";
+    this.mdFilename = options.filename
+    this.categories = []
+    this.tags = []
+    this.content = ""
+    this.slug = ""
   }
   public load() {
     // TODO SUPPORT MULTIPLE FILES
-    const filename = this.filename();
+    const filename = this.filename()
     // Get document, or throw exception on error
-    const file = fs.readFileSync(filename, "utf8");
-    const myYaml = file.split("---")[1];
+    const file = fs.readFileSync(filename, "utf8")
+    const myYaml = file.split("---")[1]
     try {
-      this.yaml = yaml.safeLoad(myYaml);
-      this.slug = path.basename(filename).split(".")[0];
-      const text = file.split("---");
+      this.yaml = yaml.safeLoad(myYaml)
+      this.slug = path.basename(filename).split(".")[0]
+      const text = file.split("---")
       // remove first empty member
-      text.shift();
+      text.shift()
       // remove yaml
-      text.shift();
+      text.shift()
       // take the rest
-      this.content = text.join("---");
-      log.debug(this);
+      this.content = text.join("---")
+      log.debug(this)
     } catch (e) {
-      log.error(e);
+      log.error(e)
     }
   }
 
   private filename(): string {
-    return `${process.env.HUGO_HOME}/content/` + this.mdFilename;
+    return `${process.env.HUGO_HOME}/content/` + this.mdFilename
   }
 }
--- src/wordpressArticle.ts
+++ src/wordpressArticle.ts
@@ -1,38 +1,38 @@
-import dotenv from "dotenv";
-import * as _ from "lodash";
-import logger from "loglevel-colored-level-prefix";
-import path from "path";
-import request from "request-promise-native";
-import util from "util";
+import dotenv from "dotenv"
+import * as _ from "lodash"
+import logger from "loglevel-colored-level-prefix"
+import path from "path"
+import request from "request-promise-native"
+import util from "util"
 
-const log = logger();
-log.setLevel("debug");
-dotenv.config();
+const log = logger()
+log.setLevel("debug")
+dotenv.config()
 
 class WordpressArticle {
-  private categories: string[];
+  private categories: string[]
   public push() {
-    const categories = await get_categories();
-    log.trace(util.inspect(categories, { colors: true }));
+    const categories = await get_categories()
+    log.trace(util.inspect(categories, { colors: true }))
 
-    article.categories = await article_categories(article, categories);
+    article.categories = await article_categories(article, categories)
 
     if (article.tags && article.tags.length > 0) {
-      article.tags = await article_tags(article);
+      article.tags = await article_tags(article)
     }
     if (article.topics && article.topics.length > 0) {
-      const topics = await article_topics(article, categories);
-      article.categories = _.union(article.categories, topics);
+      const topics = await article_topics(article, categories)
+      article.categories = _.union(article.categories, topics)
     }
   }
   public async push_to_wordpress() {
-    const response;
+    const response
     const newArticle = this.content.replace(
       /{{< youtube id="(.+)" >}}/g,
       "https://www.youtube.com/watch?v=$1",
-    );
-    this.categories = _.compact(this.categories);
-    this.tags = _.compact(this.tags);
+    )
+    this.categories = _.compact(this.categories)
+    this.tags = _.compact(this.tags)
     try {
       // disable this while we do transition
       /*
@@ -56,13 +56,13 @@ class WordpressArticle {
     })
     */
     } catch (error) {
-      log.error("error:", error); // Print the error if one occurred
+      log.error("error:", error) // Print the error if one occurred
     }
   }
 
   private async get_categories() {
-    log.debug("Let's get wordpress categories");
-    let response;
+    log.debug("Let's get wordpress categories")
+    let response
     try {
       response = await request.get({
         url: `${process.env.WP_URL}/wp-json/wp/v2/categories?per_page=100`,
@@ -70,117 +70,111 @@ class WordpressArticle {
           user: process.env.WP_USERNAME,
           password: process.env.WP_PASSWORD,
         },
-      });
+      })
     } catch (error) {
-      log.error(error); // Print the error if one occurred
+      log.error(error) // Print the error if one occurred
     }
 
-    const categories = yaml.safeLoad(response);
+    const categories = yaml.safeLoad(response)
     // console.log(util.inspect(categories, { colors:true }))
-    return categories;
+    return categories
   }
 
-private async get_tags() {
-  let response
-  try {
-     response = await request.get({
-    url: `${process.env.WP_URL}/wp-json/wp/v2/tags?per_page=100`,
-    auth: {
-        user: process.env.WP_USERNAME,
-        password: process.env.WP_PASSWORD,
-
-    },
-  })
-  } catch (error) {
-    log.error("error:", error); // Print the error if one occurred
-  }
-  const tags1 = yaml.safeLoad(response)
-
-  try {
-     response = await request.get({
-    url: `${process.env.WP_URL}/wp-json/wp/v2/tags?per_page=100&page=2`,
-    auth: {
-        user: process.env.WP_USERNAME,
-        password: process.env.WP_PASSWORD,
-
-    },
-  })
-  } catch (error) {
-    log.error("error:", error); // Print the error if one occurred
-  }
-  const tags2 = yaml.safeLoad(response)
-
-  return _.union(tags1, tags2)
-}
-
-private async article_categories(article, categories) {
-  if (article.categories && article.categories.length > 0) {
-    log.trace("continuing")
-  } else {
-    log.trace("returning empty")
-    return []
-  }
-
-  const promises = article.categories.map(async (category) => {
-    log.debug("SEARCHING category", category)
-    let hit = _.find(categories, {name: category})
-    if (! hit ) {
-      log.error(`${category} not found, creating...`)
-      hit = await create_category(category)
+  private async get_tags() {
+    let response
+    try {
+      response = await request.get({
+        url: `${process.env.WP_URL}/wp-json/wp/v2/tags?per_page=100`,
+        auth: {
+          user: process.env.WP_USERNAME,
+          password: process.env.WP_PASSWORD,
+        },
+      })
+    } catch (error) {
+      log.error("error:", error) // Print the error if one occurred
     }
-    log.debug(hit.id)
-    return hit.id
-  })
-  return Promise.all(promises)
-}
-private async article_tags(article) {
-  const tags = await get_tags()
-  // console.log("ARTICLE TAGS", article.tags)
-  const promises = article.tags.map(async (tag) => {
-    log.debug("SEARCHING for tag: ", util.inspect(tag, { colors: true }))
-    let hit = _.find(tags, {name: tag})
-    if (! hit ) {
+    const tags1 = yaml.safeLoad(response)
 
-      let response
-      try {
-        response = await request.get({
-          url: `${process.env.WP_URL}/wp-json/wp/v2/tags?search=` + tag,
-          auth: {
-                  user: process.env.WP_USERNAME,
-        password: process.env.WP_PASSWORD,
+    try {
+      response = await request.get({
+        url: `${process.env.WP_URL}/wp-json/wp/v2/tags?per_page=100&page=2`,
+        auth: {
+          user: process.env.WP_USERNAME,
+          password: process.env.WP_PASSWORD,
+        },
+      })
+    } catch (error) {
+      log.error("error:", error) // Print the error if one occurred
+    }
+    const tags2 = yaml.safeLoad(response)
 
-          },
-          json: true,
-        })
-      } catch (error) {
-        log.error("error:", error); // Print the error if one occurred
+    return _.union(tags1, tags2)
+  }
+
+  private async article_categories(article, categories) {
+    if (article.categories && article.categories.length > 0) {
+      log.trace("continuing")
+    } else {
+      log.trace("returning empty")
+      return []
+    }
+
+    const promises = article.categories.map(async (category) => {
+      log.debug("SEARCHING category", category)
+      let hit = _.find(categories, { name: category })
+      if (!hit) {
+        log.error(`${category} not found, creating...`)
+        hit = await create_category(category)
       }
-      if (response && response.length > 1 && response[0].name === tag) {
-        hit = response[0]
-      } else {
-        log.error(`${tag} not found, creating...`, response)
-        hit = await create_tag(tag)
+      log.debug(hit.id)
+      return hit.id
+    })
+    return Promise.all(promises)
+  }
+  private async article_tags(article) {
+    const tags = await get_tags()
+    // console.log("ARTICLE TAGS", article.tags)
+    const promises = article.tags.map(async (tag) => {
+      log.debug("SEARCHING for tag: ", util.inspect(tag, { colors: true }))
+      let hit = _.find(tags, { name: tag })
+      if (!hit) {
+        let response
+        try {
+          response = await request.get({
+            url: `${process.env.WP_URL}/wp-json/wp/v2/tags?search=` + tag,
+            auth: {
+              user: process.env.WP_USERNAME,
+              password: process.env.WP_PASSWORD,
+            },
+            json: true,
+          })
+        } catch (error) {
+          log.error("error:", error) // Print the error if one occurred
+        }
+        if (response && response.length > 1 && response[0].name === tag) {
+          hit = response[0]
+        } else {
+          log.error(`${tag} not found, creating...`, response)
+          hit = await create_tag(tag)
+        }
       }
-
-    }
-    log.trace(hit.id)
-    return hit.id
-  })
-  return Promise.all(promises)
-}
-private async article_topics(article, categories) {
-  const promises = article.topics.map(async (topic) => {
-    log.debug("SEARCHING for topic: ", topic)
-    let hit = _.find(categories, {name: topic})
-    if (! hit ) {
-      log.trace(categories)
-      log.error(`${topic} not found, creating...`)
-      hit = await create_category(topic)
-    }
-    log.trace(hit.id)
-    return hit.id
-  })
-  return Promise.all(promises)
-}
-
+      log.trace(hit.id)
+      return hit.id
+    })
+    return Promise.all(promises)
+  }
+  private async article_topics(article, categories) {
+    const promises = article.topics.map(async (topic) => {
+      log.debug("SEARCHING for topic: ", topic)
+      let hit = _.find(categories, { name: topic })
+      if (!hit) {
+        log.trace(categories)
+        log.error(`${topic} not found, creating...`)
+        hit = await create_category(topic)
+      }
+      log.trace(hit.id)
+      return hit.id
+    })
+    return Promise.all(promises)
+  }
 }
